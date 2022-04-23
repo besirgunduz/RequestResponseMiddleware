10c5f9be96652a64cde14fb43bb4293942e91dfe
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-04-23 23:40:29 +0300

    Add NullLogWriter

---
+++ RequestResponseMiddleware.Library/MessageCreators/LoggerFactoryMessageCreator.cs
@@ -0,0 +1,36 @@
+﻿using RequestResponseMiddleware.Library.Interfaces;
+using RequestResponseMiddleware.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestResponseMiddleware.Library.MessageCreators
+{
+    internal class LoggerFactoryMessageCreator : BaseLogMessageCreator, ILogMessageCreator
+    {
+        private readonly LoggingOptions loggingOptions;
+
+        public LoggerFactoryMessageCreator(LoggingOptions loggingOptions)
+        {
+            this.loggingOptions = loggingOptions;
+        }
+
+        public string Create(RequestResponseContext context)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var field in loggingOptions.LoggingFileds)
+            {
+                var value = GetValueByField(context, field);
+
+                sb.AppendFormat("{0}: {1}\n", field, value);
+            }
+
+            return sb.ToString();
+        }
+
+
+    }
+}
