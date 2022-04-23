d4619013fd9487a0e5c2d548c7c52ac9292608e1
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-04-23 22:39:45 +0300

    Add HandlerRequestResponseLoggingMiddleware

---
+++ RequestResponseMiddleware.Library/Middlewares/HandlerRequestResponseLoggingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using RequestResponseMiddleware.Library.Models;
+
+namespace RequestResponseMiddleware.Library.Middlewares
+{
+    public class HandlerRequestResponseLoggingMiddleware : BaseRequestResponseMiddleware
+    {
+        private readonly Func<RequestResponseContext, Task> reqResHandler;
+
+        public HandlerRequestResponseLoggingMiddleware(RequestDelegate next, Func<RequestResponseContext, Task> reqResHandler) : base(next)
+        {
+            this.reqResHandler = reqResHandler;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var reqResContext = await BaseMiddlewareInvoke(context);
+
+            await reqResHandler.Invoke(reqResContext);
+
+        }
+    }
+}
