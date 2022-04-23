d93bd4d7cea30e2d1a3eab53b3c8af8551b317ad
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-04-23 23:40:14 +0300

    Add LoggerFactoryLogWriter

---
+++ RequestResponseMiddleware.Library/LogWriters/LoggerFactoryLogWriter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using RequestResponseMiddleware.Library.Interfaces;
+using RequestResponseMiddleware.Library.MessageCreators;
+using RequestResponseMiddleware.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestResponseMiddleware.Library.LogWriters
+{
+    internal class LoggerFactoryLogWriter : ILogWriter
+    {
+        private readonly ILogger logger;
+        private readonly LoggingOptions loggingOptions;
+
+        public ILogMessageCreator MessageCreator { get; }
+
+        internal LoggerFactoryLogWriter(ILoggerFactory loggerFactory, LoggingOptions loggingOptions)
+        {
+            logger = loggerFactory.CreateLogger(loggingOptions.LoggerCategoryName);
+            this.loggingOptions = loggingOptions;
+
+            MessageCreator = new LoggerFactoryMessageCreator(loggingOptions);
+        }
+
+
+        public async Task Write(RequestResponseContext context)
+        {
+            var message = MessageCreator.Create(context);
+
+            logger.Log(loggingOptions.LogLevel, message);
+
+            await Task.CompletedTask;
+        }
+    }
+}
